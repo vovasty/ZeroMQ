0b339b8f2bd3adbd9f183d1713f5f0d7f17ab8c5
Vladimir Solomenchuk <vlad@aramzamzam.net>
2016-10-05 10:53:37 -0700

    fixed tests

--- Tests/MainTests/ZeroMQTests.swift
+++ Tests/MainTests/ZeroMQTests.swift
@@ -2,27 +2,24 @@ import XCTest
 @testable import ZeroMQ
 
 class ZeroMQTests: XCTestCase {
-    func testPushPull() {
+    func testPushPull() throws {
         var called = false
-        do {
-            let context = try Context()
+        let context = try Context()
 
-            let inbound = try context.socket(.Pull)
-            try inbound.bind("tcp://127.0.0.1:5555")
+        let inbound = try context.socket(.pull)
+        try inbound.bind("tcp://127.0.0.1:5555")
 
-            let outbound = try context.socket(.Push)
-            try outbound.connect("tcp://127.0.0.1:5555")
+        let outbound = try context.socket(.push)
+        try outbound.connect("tcp://127.0.0.1:5555")
 
-            try outbound.sendString("Hello World!")
-            try outbound.sendString("Bye!")
+        XCTAssert(try outbound.sendString("Hello World!"))
+        XCTAssert(try outbound.sendString("Bye!"))
 
-            while let data = try inbound.receiveString() , data != "Bye!" {
-                called = true
-                XCTAssert(data == "Hello World!")
-            }
-        } catch {
-            XCTAssert(false)
+        while let data = try inbound.receiveString() , data != "Bye!" {
+            called = true
+            XCTAssert(data == "Hello World!")
         }
+        
         XCTAssert(called)
     }
 }
